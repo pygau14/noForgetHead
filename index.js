58517e856ff2f3d83585046929273faf07065b9f
Gaurav <pygau14@Gauravs-MacBook-Pro.local>
2023-10-05 14:22:33 +0530

    updated index.js

--- index.js
+++ index.js
@@ -5,36 +5,55 @@ const mysql = require('mysql2/promise');
 // Create an Express application
 const app = express();
 
+// username = db-a1x3ljk38bcm 
+//  password = wylT8ld3xTVwMLST7KL3XKkg 
+//  host = up-pl-waw1-mysql-1.db.run-on-erla.com 
+//  port = 11550
+
 // Define the database connection
 const dbConfig = {
-  host: 'your_mysql_host',
-  user: 'your_mysql_user',
-  password: 'your_mysql_password',
-  database: 'your_database_name'
+  host: 'up-pl-waw1-mysql-1.db.run-on-erla.com',
+  user: 'db-a1x3ljk38bcm',
+  password: 'wylT8ld3xTVwMLST7KL3XKkg',
+  database: 'noForgetHeadDB',
+  port : 11550,
 };
 
 // Create a pool of database connections
 const pool = mysql.createPool(dbConfig);
 
-// Define a route for API-1 to test server functionality
-app.get('/api1', async (req, res) => {
-  try {
-    // Get a database connection from the pool
-    const connection = await pool.getConnection();
+// // Define a route for API-1 to test server functionality
+// app.get('/api1', async (req, res) => {
+//   try {
+//     // Get a database connection from the pool
+//     const connection = await pool.getConnection();
 
-    // Execute a simple query to test the database connection
-    const [rows, fields] = await connection.query('SELECT "API-1 is working" AS message');
+//     // Execute a simple query to test the database connection
+//     const [rows, fields] = await connection.query('SELECT "API-1 is working" AS message');
 
-    // Release the database connection back to the pool
-    connection.release();
+//     // Release the database connection back to the pool
+//     connection.release();
 
-    // Send a response to the client
-    res.json({ message: rows[0].message });
-  } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+//     // Send a response to the client
+//     res.json({ message: rows[0].message });
+//   } catch (error) {
+//     console.error('Error:', error.message);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+//Test API 
+app.get('/test',(req,res)=>{
+  res.status(200).send("Server is working - test AC1");
+})
+
+//Test API 2 - JSON
+app.post("/test2",(req,res)=>{
+  const num = req.body.num;
+
+  res.status(200).json({number : num});
+
+})
 
 // Start the server on a specified port
 const PORT = process.env.PORT || 3000;
